21cbaaeee7b2d29b7e34a8111f3681d7b8cec21a
Ar-Ray-code <ray255ar@gmail.com>
2022-05-22 14:50:05 +0900

    add comment

--- README.md
+++ README.md
@@ -39,17 +39,21 @@ See [trigger_example.cpp](./src/trigger_example.cpp) .
 
 int main(void)
 {
+    // ROS2 init
     rclcpp::init(0, nullptr);
     rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("trigger_example");
 
-    // Define only (trigger)
+    // Define trigger2
     Trigger trigger2(node, "signal2");
 
-    // Define and running Trigger
+    // Set trigger1 and wait. (Setting subscription "/signal1" and spin_some)
     Trigger trigger1(node, "signal1", 10, true);
+    // Execute after receiving "signal1"
     std::cout << "Step1" << std::endl;
 
+    // wait by trigger2 (Setting subscription "/signal2" and spin_some)
     trigger2.wait_until_callback();
+    // Execute after receiving "/signal2"
     std::cout << "Step2" << std::endl;
 
     rclcpp::shutdown();
--- src/trigger_example.cpp
+++ src/trigger_example.cpp
@@ -2,15 +2,21 @@
 
 int main(void)
 {
+    // ROS2 init
     rclcpp::init(0, nullptr);
     rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("trigger_example");
 
-    Trigger trigger1(node, "signal2");
+    // Define trigger2
+    Trigger trigger2(node, "signal2");
 
-    Trigger trigger(node, "signal1", 10, true);
+    // Set trigger1 and wait. (Setting subscription "/signal1" and spin_some)
+    Trigger trigger1(node, "signal1", 10, true);
+    // Execute after receiving "signal1"
     std::cout << "Step1" << std::endl;
 
-    trigger1.wait_until_callback();
+    // wait by trigger2 (Setting subscription "/signal2" and spin_some)
+    trigger2.wait_until_callback();
+    // Execute after receiving "/signal2"
     std::cout << "Step2" << std::endl;
 
     rclcpp::shutdown();
